bc07f5951256e1b42cdc7f7c68a079da5692f75b
Dmitry-Wsl <dev@dergunov.info>
2023-07-03 21:15:03 +0300

    AppServiceProvider изменен ChatGPT

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,16 +1,16 @@
 <?php
-
 namespace App\Providers;
 
+use Awcodes\Curator\CurationPreset;
+use Awcodes\Curator\Facades\Curator;
+use Filament\Facades\Filament;
 use Illuminate\Foundation\Vite;
 use Illuminate\Support\ServiceProvider;
-
+use Z3d0X\FilamentFabricator\Facades\FilamentFabricator;
+use Z3d0X\FilamentFabricator\Forms\Components\PageBuilder;
 
 class AppServiceProvider extends ServiceProvider
 {
-    private static bool $pageBuilderConfigured = false;
-    private static bool $curatorPresetsSet = false;
-
     /**
      * Register any application services.
      */
@@ -18,7 +18,6 @@ class AppServiceProvider extends ServiceProvider
     {
         //
     }
-
     /**
      * Bootstrap any application services.
      */
@@ -26,47 +25,54 @@ class AppServiceProvider extends ServiceProvider
     {
         $vite = app(Vite::class)('resources/css/app.css');
 
-        // Тема filament
-        \Filament\Facades\Filament::serving(function () use ($vite) {
-            // Using Vite
-            \Filament\Facades\Filament::registerTheme($vite);
+        Filament::serving(function () use ($vite) {
+            Filament::registerTheme($vite);
         });
 
-        // Загрузка ресурсов
-        \Z3d0X\FilamentFabricator\Facades\FilamentFabricator::registerStyles([
-            // 'https://unpkg.com/tippy.js@6/dist/tippy.css',//external url
-            $vite, //vite
-            // asset('css/app.css'),// asset from public folder
+        $this->registerStyles($vite);
+
+        $this->configurePageBuilder();
+
+        $this->setCuratorPresets();
+
+    }
+
+    private function registerStyles(string $vite): void
+    {
+        FilamentFabricator::registerStyles([
+            $vite,
         ]);
+    }
 
-        // Настройки PageBuilder
-        if (!self::$pageBuilderConfigured) {
-            \Z3d0X\FilamentFabricator\Forms\Components\PageBuilder::configureUsing(function (\Z3d0X\FilamentFabricator\Forms\Components\PageBuilder $builder) {
-                $builder->collapsed(); //You can use any method supported by the Builder field
-            });
-            self::$pageBuilderConfigured = true;
-        }
+    private function configurePageBuilder(): void
+    {
+        PageBuilder::configureUsing(function (PageBuilder $builder) {
+            $builder->collapsed();
+        });
+    }
 
-        // Настройки Curation Preset
-        if (!self::$curatorPresetsSet) {
-            \Awcodes\Curator\Facades\Curator::curationPresets([
-                // настройки пресетов...
-                \Awcodes\Curator\CurationPreset::make('thumbnail')
-                    ->label('Thumbnail')
-                    ->width(200)
-                    ->height(200)
-                    ->format('webp')
-                    ->quality(80),
-                \Awcodes\Curator\CurationPreset::make('hero')
-                    ->label('Hero')
-                    ->width(960)
-                    ->height(300),
-                \Awcodes\Curator\CurationPreset::make(name: 'og-image')
-                    ->label('OG Image')
-                    ->width(1200)
-                    ->height(630),
-            ]);
-            self::$curatorPresetsSet = true;
-        }
+    private function setCuratorPresets(): void
+    {
+        Curator::curationPresets($this->getCurationPresets());
+    }
+
+    private function getCurationPresets(): array
+    {
+        return [
+            CurationPreset::make('thumbnail')
+                ->label('Thumbnail')
+                ->width(200)
+                ->height(200)
+                ->format('webp')
+                ->quality(80),
+            CurationPreset::make('hero')
+                ->label('Hero')
+                ->width(960)
+                ->height(300),
+            CurationPreset::make(name: 'og-image')
+                ->label('OG Image')
+                ->width(1200)
+                ->height(630),
+        ];
     }
 }
